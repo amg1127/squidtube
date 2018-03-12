98a30b29ed0054e4143a0d5e7d66bfc10749dbd4
Anderson M. Gomes <amg1127@gmail.com>
2018-03-12 01:21:35 -0300

    Still coding.

---
+++ src/jobworker.cpp
@@ -0,0 +1,6 @@
+#include "jobworker.h"
+
+jobWorker::jobWorker()
+{
+
+}
---
+++ src/jobworker.h
@@ -0,0 +1,11 @@
+#ifndef JOBWORKER_H
+#define JOBWORKER_H
+
+
+class jobWorker : public QThread
+{
+public:
+    jobWorker();
+};
+
+#endif // JOBWORKER_H
